42eed6088b76c585d15c1dd4fb45ad622e79a7a0
ashevin <avi.shevin@kik.com>
2017-11-27 13:01:46 +0200

    Remove unused parameter.

--- CoreDataStack/CoreDataStack/Source/CoreDataStack.swift
+++ CoreDataStack/CoreDataStack/Source/CoreDataStack.swift
@@ -16,6 +16,14 @@ public enum CoreDataStackError: Error {
 public typealias CoreDataManagerUpdateBlock = (NSManagedObjectContext, inout Bool) -> ()
 public typealias CoreDataManagerQueryBlock = (NSManagedObjectContext) -> ()
 
+/**
+ `CoreDataStack` is a safe, simple, and easy-to-use Core Data stack.  The focus is on making safe
+ Core Data the easy path, thereby avoiding common bugs associated with using multiple contexts.
+
+ `CoreDataStack` performs modifications in a background queue, and offers read-only access to
+ the main thread.
+ */
+
 public final class CoreDataStack {
     public let viewContext: ReadOnlyMOC
 
@@ -26,10 +34,21 @@ public final class CoreDataStack {
     private var token: AnyObject? = nil
     private var isShuttingDown = false
 
+    /**
+     Create a stack with the given model name, with a store type of `NSSQLiteStoreType`.
+
+     - parameter modelName: The model name within the resource bundle
+     */
     convenience public init(modelName: String) throws {
         try self.init(modelName: modelName, storeType: NSSQLiteStoreType)
     }
 
+    /**
+     Create a stack with the given model name and store type.
+
+     - parameter modelName: The model name within the resource bundle
+     - parameter storeType: One of the persistent store types supported by Core Data
+     */
     public init(modelName: String, storeType: String) throws {
         guard let model = CoreDataStack.model(for: modelName) else {
            throw CoreDataStackError.missingModelName
@@ -109,6 +128,12 @@ public final class CoreDataStack {
 }
 
 extension CoreDataStack {
+    /**
+     Shuts down the stack safely, waiting for all operations to complete.  The stack is not usable
+     after this method returns.
+
+     This method must be called from the main thread.
+     */
     public func shutdown() {
         guard Thread.current == Thread.main else {
             fatalError("Must call from main thread.")
@@ -125,6 +150,9 @@ extension CoreDataStack {
         queue.waitUntilAllOperationsAreFinished()
     }
 
+    /**
+     A safe version of the `save()` method; throws when called on the `viewContext`.
+     */
     public func save(_ context: NSManagedObjectContext) throws {
         guard context != viewContext else {
             fatalError("Saving the viewContext is illegal.")
@@ -135,6 +163,18 @@ extension CoreDataStack {
         }
     }
 
+    /**
+     Submits a block to a background context.  The block receives the context and an `inout` boolean
+     that may be set to `false` to discard changes to the context.  An optional completion block may
+     be provided.
+
+     **Notes**: The provided context is fresh; managed objects must be loaded into the context prior
+     to use.  The `completion` block, if provided, is called from a background thread.  Dispatch work
+     to the main thread if necessary.
+
+     - parameter block: A block which performs modifications to Core Data entities
+     - parameter completion: A block which is invoked after the context has been saved.
+     */
     public func perform(_ block: @escaping CoreDataManagerUpdateBlock, completion: (() -> ())? = nil) {
         guard isShuttingDown == false else {
             return
@@ -158,6 +198,15 @@ extension CoreDataStack {
         }
     }
 
+    /**
+     Submits a block to a background context.  The block receives the context, which may be used
+     to query Core Data, but not to modify the store.
+
+     **Notes**: The provided context is fresh; managed objects must be loaded into the context prior
+     to use.
+
+     - parameter block: A block which performs queries of Core Data entities
+     */
     public func query(_ block: @escaping CoreDataManagerQueryBlock) {
         guard isShuttingDown == false else {
             return
@@ -174,6 +223,13 @@ extension CoreDataStack {
         }
     }
 
+    /**
+     Submits a block to the `viewContext`.  The block receives the context, which may be used
+     to query Core Data, but not to modify the store.  This method may be called from a
+     background thread.
+
+     - parameter block: A block which performs queries of Core Data entities
+     */
     public func viewQuery(_ block: CoreDataManagerQueryBlock) {
         guard isShuttingDown == false else {
             return
@@ -192,6 +248,9 @@ extension CoreDataStack {
 //MARK: - Public - NSManagedObject -
 
 public extension NSManagedObject {
+    /**
+        Provides the entity's name, used with `NSFetchRequest`.
+     */
     public static var entityName: String {
         return self.entity().name ?? String(describing: type(of: self))
     }
@@ -200,14 +259,27 @@ public extension NSManagedObject {
 //MARK: - Public - NSManagedObjectContext -
 
 public extension NSManagedObjectContext {
-    public func itemsMatching<T : NSManagedObject>(conditions: [String: Any],
-                                                   for entity: T.Type) throws -> [T] {
+    /**
+     Loads items matching the given conditions [see `NSPredicate+extensions`] into the context.
+
+     - parameter conditions: A dictionary of conditions which will be converted into an `NSPredicate`
+
+     - returns: An array of type `T`.
+     */
+    public func itemsMatching<T : NSManagedObject>(conditions: [String: Any]) throws -> [T] {
         let request = NSFetchRequest<NSManagedObject>(entityName: T.entityName)
         request.predicate = NSPredicate.predicate(for: conditions)
 
         return try self.fetch(request) as! [T]
     }
 
+    /**
+     Loads an array of items into the context.
+
+     - parameter items: The array of items to load.
+
+     - returns: An array of type `T`.
+     */
     public func load<T : NSManagedObject>(items: [T]) throws -> [T] {
         let request = NSFetchRequest<NSManagedObject>(entityName: T.entityName)
         request.predicate = NSPredicate(format: "SELF IN %@", argumentArray: [items])
@@ -215,10 +287,24 @@ public extension NSManagedObjectContext {
         return try self.fetch(request) as! [T]
     }
 
+    /**
+     Loads an item into the context.
+
+     - parameter item: The item to load.
+
+     - returns: An `NSManagedObject` of type `T`.
+     */
     public func load<T : NSManagedObject>(item: T) throws -> T {
         return try existingObject(with: item.objectID) as! T
     }
 
+    /**
+     Loads an item into the context.
+
+     - parameter objectID: The `NSManagedObjectID` to load.
+
+     - returns: An `NSManagedObject` of type `T`.
+     */
     public func load<T : NSManagedObject>(objectID: NSManagedObjectID) throws -> T {
         return try existingObject(with: objectID) as! T
     }
